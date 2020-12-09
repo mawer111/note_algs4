9fc8e2175a72f277a5b6bc74d53801d17a8cf3d1
mawei@lichendt.com <mawei@lensung.com>
2020-12-09 14:42:28 +0800

    init

--- src/main/java/study/ss/book/algs4/std/StdDrawDemo.java
+++ src/main/java/study/ss/book/algs4/std/StdDrawDemo.java
@@ -1,8 +1,8 @@
 package study.ss.book.algs4.std;
 
-import study.ss.book.algs4.std.StdDraw;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class StdDrawDemo {
 
@@ -18,19 +18,19 @@ public class StdDrawDemo {
         for (int i = 0; i < N; i++) {
             a[i] = StdRandom.uniform();
         }
-
+        Arrays.sort(a);
         for (int i = 0; i < N; i++) {
-            double x = i / N;
-            double y = a[i];
+            double x = 1.0 * i / N;
+            double y = a[i] / 2.0;
             double rw = 0.5 / N;
-            double rh = a[i];
+            double rh = a[i] / 2.0;
 
             StdDraw.filledRectangle(x, y, rw, rh);
         }
 
     }
 
-    public static void drawSqrt() {
+    public static void drawMathFunction() {
         int N = 100;
         StdDraw.setXscale(0, N);
         StdDraw.setYscale(0, N * N);
