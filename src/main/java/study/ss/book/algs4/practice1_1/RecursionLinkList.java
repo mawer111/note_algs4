c40787c3633020f46303d0efd034236cf06275c9
mawei@lichendt.com <mawei@lensung.com>
2020-12-11 10:12:38 +0800

    init

--- src/main/java/study/ss/book/algs4/practice1_1/RecursionLinkList.java
+++ src/main/java/study/ss/book/algs4/practice1_1/RecursionLinkList.java
@@ -2,25 +2,77 @@ package study.ss.book.algs4.practice1_1;
 
 import study.ss.book.algs4.std.StdOut;
 
+import java.util.ArrayList;
+
 public class RecursionLinkList {
 
 
     public static void main(String[] args) {
         Node n1 = assemblyOrderNode(4);
+        Node t = n1;
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        while (t != null) {
+            nodes.add(t);
+            t = t.next;
+        }
         print(n1);
         StdOut.println();
-        recursionNode(n1);
-        print(n1);
+        Node node = reverseNode(n1);
+        print(node);
     }
 
+    /**
+     * 循环翻转链表
+     * node指向当前节点，将node遍历过的视为已经翻转过的节点，所以只需要将当前节点的next指向之前的节点
+     *
+     * node
+     * ⬇️
+     * 1 -> 2 -> 3 -> 4
+     *
+     * node                  node
+     * ⬇️                     ⬇️
+     * 2 -> 3 -> 4   ---->    3 -> 4 -> null
+     *
+     * pre                   pre
+     * ⬇️                     ⬇️
+     * 1 -> null     ---->    2 -> 1 -> null
+     *
+     *
+     *
+     *
+     * 循环翻转链表
+     * @param node
+     * @param <T>
+     * @return
+     */
+    public static <T> Node<T> reverseNode(Node<T> node) {
+        Node pre = null;
+        while (node != null) {
+            Node t = node.next;
+            node.next = pre;
+            pre = node;
+            node = t;
+        }
+        return pre;
+    }
+
+
+    /**
+     * 递归翻转链表
+     * @param node
+     * @param <T>
+     * @return
+     */
     public static <T> Node<T> recursionNode(Node<T> node) {
         if (node == null || node.next == null) {
             return node;
         }
         Node newNode = recursionNode(node.next);
-        newNode.next = node;
-//        print(newNode);
-        return node;
+        Node t = node.next;
+        t.next = node;
+        node.next = null;
+
+        return newNode;
     }
 
     static class Node<T> {
