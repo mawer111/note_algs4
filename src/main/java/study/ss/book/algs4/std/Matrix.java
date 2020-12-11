ca8a0a9290561939e240d9b97d5f99cd087319bc
mawei@lichendt.com <mawei@lensung.com>
2020-12-11 17:16:23 +0800

    practice 1.1.33 matrix

---
+++ src/main/java/study/ss/book/algs4/std/Matrix.java
@@ -0,0 +1,25 @@
+package study.ss.book.algs4.std;
+
+public class Matrix {
+
+
+    /**
+     * 向量点乘
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public static double dot(double[] x, double[] y) {
+        double result = .0f;
+        for (int i = 0; i < x.length; i++) {
+            result = result + x[i] * y[i];
+        }
+        return result;
+    }
+
+
+
+
+
+}
