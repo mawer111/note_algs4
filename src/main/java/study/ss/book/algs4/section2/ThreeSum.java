d1e9c967d0aa4e30800e9eafc0299de237536d9f
mawei@lichendt.com <mawei@lensung.com>
2020-12-22 17:42:20 +0800

    DoubingRatio

--- src/main/java/study/ss/book/algs4/section2/ThreeSum.java
+++ src/main/java/study/ss/book/algs4/section2/ThreeSum.java
@@ -14,7 +14,7 @@ public class ThreeSum {
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 for (int k = j + 1; k < a.length; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
+                    if (a[i] + a[j] == -a[k]) {
                         cnt++;
                     }
                 }
